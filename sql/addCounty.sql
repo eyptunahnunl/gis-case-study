a1cc99757b36a370a8b726b99f446f20b448b012
eyptunahnunl <e.tunahan.unal.05@gmail.com>
2023-08-26 19:50:23 +0300

    geoserver and sql

--- sql/addCounty.sql
+++ sql/addCounty.sql
@@ -1,7 +1,7 @@
 
-alter table applicants 
-add column city varchar
+--a. Tüm tablolara bir kolon açılarak bu kolona ilgili İlçe’nin bilgileri aktarılmalıdır.
 
+-- başvuru tablosuna ilçe bilgilerinin eklenmesi
 UPDATE applicants AS a
 SET city = t.adi
 FROM (
@@ -9,3 +9,34 @@ FROM (
     FROM county
 ) AS t
 WHERE ST_Intersects(a.geometry, t.geometry);
+
+-- nöbetçi eczaneler için 
+
+update pharmacy_on_duty as p 
+set dist = t.adi
+from (
+	 SELECT adi, st_transform(geometry, 4326) AS geometry
+    FROM county
+) as t 
+where ST_Intersects(p.geometry, t.geometry);
+
+-- maden tablosu için 
+
+update miningarea as m 
+set city = t.adi
+from (
+	 SELECT adi, st_transform(geometry, 23036) AS geometry
+    FROM county
+) as t 
+where ST_Intersects(m.geometry, t.geometry);
+
+
+-- b.Nöbetçi Eczaneler verisinin İlçelerin merkez noktalarına olan uzakları hesaplanmalıdır.
+
+select p.name,t.adi, st_distance(p.geometry,t.geometry) from pharmacy_on_duty p, 
+(SELECT adi, st_centroid(st_transform(geometry, 4326)) AS geometry
+    FROM county) t 
+    order by t.adi
+
+---c. Başvuran Kişi verilerinin Nöbetçi Eczanelere olan uzaklıkları hesaplanmalıdır. 
+select a.name, p.name, st_distance(a.geometry, p.geometry) from applicants a, pharmacy_on_duty p
